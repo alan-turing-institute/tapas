73cda8a44e35dc0d2c0b2d35098110d9fc7793e0
James Jordon <jjordon95@gmail.com>
2022-03-30 12:26:35 +0100

    Initial commit of API
    Generator base class, Classifier base class, Groundhog class
    
    Initial commit of run_groundhog.py
    
    run_groundhog.py is an example script for how each of the parts might work together
    
    Added init file for tests

---
+++ attacks/groundhog_attack.py
@@ -0,0 +1,114 @@
+"""
+Parent class for launching a membership inference attack on the output of a 
+generative model
+"""
+import numpy as np
+import pandas as pd
+
+from attack_models.base_classes import MIAttack
+from utils.data import read_meta
+
+from warnings import simplefilter
+simplefilter('ignore', category=FutureWarning)
+simplefilter('ignore', category=DeprecationWarning)
+
+import multiprocessing as mp
+
+class Groundhog(MIAttack):
+    """
+    Parent class for membership inference attack on the output of a 
+    generative model using a classifier. A single instance of this class
+    corresponds to a specific target data point.
+
+    Args:
+        Classifier : Classifier to use to distinguish synthetic datasets
+        metadata (dict) : Metadata dictionary describing data
+        quids (list) : List of column names to be regarded as quasi-identifiers.
+            This list makers the Attack class aware of which columns that would
+            usually be continuous are going to be categorical (binned).
+    """
+    def __init__(self,
+                 Classifier,
+                 data_description,
+                 quids=None
+                 ):
+        self.Classifier = Classifier
+        self.data_description = data_description
+
+        self.trained = False
+
+        self.__name__ = f'{self.Classifier.__class__.__name__}Groundhog'
+
+    def train(self, datasets, labels):
+        """
+        Train the attack classifier on a labelled training set
+
+        Args:
+            datasets (List[pd.DataFrame]): List of synthetic datasets to use
+                as training data
+            labels (np.ndarray): Labels for datasets indicating whether or not
+                the target was present in the training data that produced each
+                synthetic dataset
+        """
+        # Fit the classifier to the data
+        self.Classifier.fit(synthetic_datasets, labels)
+
+        self.trained = True
+
+    def attack(self, datasets):
+        """
+        Make a guess about the target's membership in the training data of
+        each of the generative models that produced each of the synthetic
+        input datasets
+
+        Args:
+            datasets (List[pd.DataFrame]) : List of synthetic datasets to attack
+            attemptLinkage (bool) : If True, search for Target explicitly in
+                each dataset before using the classifier
+            target : Target to find if attemptLinkage=True
+
+        Returns:
+            guesses (List[int]) : List of guesses. The ith entry corresponds
+                to the guess for the ith dataset in datasets. Guess of 0
+                corresponds to target not present.
+        """
+        assert self.trained, 'Attack must first be trained.'
+
+        guesses = []
+        for df in datasets:
+            guess = self._make_guess(df)
+            guesses.append(guess)
+        return guesses
+
+    def _make_guess(self, df):
+        """
+        Make a guess for a single dataset about the presence of the target in
+        the training data that generated the dataset
+        """
+        return np.round(self.Distinguisher.predict(f), 0).astype(int)[0]
+
+    def get_confidence(self, synT, secret):
+        """
+        Calculate classifier's raw probability about the presence of the target.
+        Output is a probability in [0, 1].
+
+        Args:
+            synT (List[pd.DataFrame]) : List of dataframes to predict
+            secret (List[int]) : List indicating the truth of target's presence
+                in (training data of) corresponding dataset in synT
+
+        Returns:
+            List of probabilities corresponding to attacker's guess about the truth
+        """
+        assert self.trained, 'Attack must first be trained.'
+        if self.FeatureSet is not None:
+            synT = np.stack([self.FeatureSet.extract(s) for s in synT])
+        else:
+            if isinstance(synT[0], pd.DataFrame):
+                synT = np.stack([convert_df_to_array(s, self.metadata).flatten() for s in synT])
+            else:
+                synT = np.stack([s.flatten() for s in synT])
+
+        probs = self.Classifier.predict_proba(synT)
+
+        return [p[s] for p,s in zip(probs, secret)]
---
+++ run_groundhog.py
@@ -0,0 +1,82 @@
+from datetime import datetime
+import os
+
+from attacks.set_classifiers import LRClassifier, SetReprClassifier, NaiveRep
+from attack_models import Groundhog
+from datasets import TabularDataset
+
+from generative_models import ReturnRaw
+
+# Set some parameters
+num_train_samples = 20
+num_test_samples = 10
+train_data_size = 1000
+synthetic_data_size = 1000
+
+# Set output directory
+curr_time = datetime.now().strftime('%d%m_%H%M%S')
+save_dir = os.path.join('runs', curr_time)
+
+# Load data
+dataset = TabularDataset.read('data/texas')
+
+# Initialise synthetic data generator
+sdg_model = ReturnRaw() # instance of generators.Generator
+
+# Set target
+# TODO: What should target be? DF? Dataset instance?
+# Should there be a separate sample_row method on Dataset?
+target = dataset.sample(1)
+
+
+# Create training datasets
+# TODO: Find home
+def create_synthetic_from_neighbouring(generator,
+                                       dataset,
+                                       target,
+                                       num_samples,
+                                       train_size,
+                                       syn_size):
+    """
+    Generate synthetic datasets from pairs of neighbouring training datasets
+    """
+    # Split dataset
+    datasets = dataset.split(num_splits=num_samples,
+                             split_size=train_size,
+                             drop=target)
+
+    synthetic_datasets_no_T = []
+    synthetic_datasets_with_T = []
+
+    for dataset in datasets_without_T:
+        synthetic_datasets_no_T.append(generator(dataset, syn_size))
+
+        dataset.add(target)
+        synthetic_datasets_with_T.append(generator(dataset, syn_size))
+
+    labels = ([0] * num_samples) + ([1] * num_samples)
+    synthetic_datasets = synthetic_datasets_no_T + synthetic_datasets_with_T
+
+    return synthetic_datasets, labels
+
+train_datasets, train_labels = create_synthetic_from_neighbouring(
+    sdg_model, dataset, target, num_train_samples, train_data_size, synthetic_data_size
+)
+
+# Initialise attack model
+classifier = SetReprClassifier(NaiveRep, LRClassifier, dataset.description)
+attack = Groundhog(classifier, dataset.description)
+
+# Train attack model
+attack.train(train_datasets, train_labels)
+
+# Generate test data
+test_datasets, test_labels = create_synthetic_from_neighbouring(
+    sdg_model, dataset, target, num_test_samples, train_data_size, synthetic_data_size
+)
+
+# Predict with attack
+predictions = attack.attack(test_datasets)
+
+# Do something with predictions
+print(f'Accuracy: {np.mean(np.array(predictions) == np.array(test_labels))}')
