0626521d60af5f737ab31626ac2a56c7247c7ad3
James Jordon <jjordon95@gmail.com>
2022-04-07 15:14:36 +0100

    Added basic utils for set classification
    Added get_num_features and encode_data, both derived from the original Stadler codebase. These will need to be modified for whichever data_description we settle on.

--- attacks/set_classifiers.py
+++ attacks/set_classifiers.py
@@ -28,10 +28,10 @@ class SetReprClassifier(SetClassifier):
     Classifier that first computes a representation of the dataset and then
     uses 'traditional' (vector-based) classification techniques
     """
-    def __init__(self, SetRep, Classifier, meta_dict):
-        self.SetRep = SetRep(meta_dict)
-        self.Classifier = Classifier(self.SetRep.meta_dict_out)
-        self.meta_dict = meta_dict
+    def __init__(self, SetRep, Classifier, data_description):
+        self.SetRep = SetRep(data_description)
+        self.Classifier = Classifier(self.SetRep.data_description_out)
+        self.data_description = data_description
 
     def fit(datasets, labels):
         representations = self.SetRep(datasets)
@@ -44,8 +44,8 @@ class SetReprClassifier(SetClassifier):
 
 class LRClassifier:
     """Logistic regresison classifier"""
-    def __init__(self, meta_dict):
-        self.num_features = meta_dict['num_features']
+    def __init__(self, data_description):
+        self.num_features = data_description['num_features']
         self.Classifier = LogisticRegression()
 
     def fit(self, X, y):
@@ -61,12 +61,12 @@ class NaiveRep: # TODO: Write tests
     Naive feature set from Stadler et al. paper. Mean, median, and variance 
     of each column is computed.
     """
-    def __init__(self, meta_dict):
-        self.meta_dict = meta_dict
-        self.meta_dict_out = {'num_features': 3*get_num_features(self.meta_dict)}
+    def __init__(self, data_description):
+        self.data_description = data_description
+        self.data_description_out = {'num_features': 3*get_num_features(self.data_description)}
 
     def get_representations(self, datasets):
-        np_data = [encode_data(dataset, self.meta_dict) for dataset in datasets]
+        np_data = [encode_data(dataset, self.data_description) for dataset in datasets]
         representations = np.stack(
             [np.concatenate(
                 [np.nanmean(data, axis=0), np.nanmedian(data, axis=0), np.nanvar(data, axis=0)]
