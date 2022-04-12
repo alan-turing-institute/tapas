9e72ae054e449cb3c6d3bf7bed25c550305e8b64
James Jordon <jjordon95@gmail.com>
2022-04-12 13:21:49 +0100

    More lower-casing

--- prive/tests/test_dataset.py
+++ prive/tests/test_dataset.py
@@ -5,7 +5,7 @@ from warnings import filterwarnings
 
 filterwarnings('ignore')
 
-from privE.datasets.dataset import TabularDataset
+from prive.datasets.dataset import TabularDataset
 
 
 class TestTabularDataset(TestCase):
