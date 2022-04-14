262aa9a17303a805d1acdd5602078d6f5c00f012
James Jordon <jjordon95@gmail.com>
2022-04-14 11:13:51 +0100

    Update direct_linkage.py

--- prive/attacks/direct_linkage.py
+++ prive/attacks/direct_linkage.py
@@ -1,4 +1,7 @@
-"""A direct linkage attack looks up the target record in the synthetic data."""
+"""
+A direct linkage attack looks up the target record in the synthetic data.
+
+"""
 
 from .base_classes import Attack
 
@@ -6,14 +9,17 @@ from .base_classes import Attack
 class DirectLinkage(Attack):
     """
     Attack that checks only whether or not the target is in the generated synthetic
-     dataset or not.
+    dataset or not.
+
     """
     def __init__(self, threat_model):
-        Attack.__init__(self, threat_model)
         self.target_record = target_record
 
     def attack(self, datasets):
-        return [(df==self.target_record).all(1).any() for df in datasets]
+        return [(dataset.data==self.target_record).all(1).any() for dataset in datasets]
 
     def attack_score(self, datasets):
         return self.attack(datasets)
+
+    def train(self):
+        pass
