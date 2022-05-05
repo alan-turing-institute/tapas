8f54526f2320b9edb633edcf8694d77d5eb0d806
crangelsmith <crangelsmith@turing.ac.uk>
2022-05-05 12:26:20 +0100

    adding tp and fp metrics

--- prive/report/report.py
+++ prive/report/report.py
@@ -35,16 +35,52 @@ class MiAttackSummary(AttackSummary):
         attack_info
         """
 
-        self.labels = labels
-        self.predictions = predictions
+        self.labels = np.array(labels)
+        self.predictions = np.array(predictions)
         self.generator_info = generator_info
         self.attack_info = attack_info
 
     @property
     def accuracy(self):
-        return np.mean(np.array(self.predictions) == np.array(self.labels))
+        """
+        Accuracy of the attacks based on the rate of correct predictions.
 
-    def calculate_metrics(self, labels, predictions, generator_info, attack_info):
+        Returns
+        -------
+        float
+
+        """
+        return np.mean(self.predictions == self.labels)
+
+    @property
+    def tp(self):
+        """
+        True positives based on rate of attacks where the target is correctly inferred
+        as being in the sample.
+
+        Returns
+        -------
+        float
+
+        """
+        targetin = np.where(self.labels == 1)[0]
+        return np.sum(self.predictions[targetin] == 1)/len(targetin)
+
+    @property
+    def fp(self):
+        """
+        False positives based on rate of attacks where the target is incorrectly inferred
+        as being in the sample.
+
+        Returns
+        -------
+        float
+
+        """
+        targetout = np.where(self.labels == 0)[0]
+        return np.sum(self.predictions[targetout] == 1) / len(targetout)
+
+    def calculate_metrics(self, labels, predictions):
         pass
 
     def write(self):
--- prive/tests/test_report.py
+++ prive/tests/test_report.py
@@ -6,9 +6,15 @@ from prive.report import MiAttackSummary
 class MiAttackSummaryTest(unittest.TestCase):
     def setUp(self):
         np.random.seed(0)
-        self.predictions = list(np.random.randint(2, size=100))
-        self.labels = list(np.random.randint(2, size=100))
+        self.predictions = (np.random.randint(2, size=100))
+        self.labels = (np.random.randint(2, size=100))
         self.attack_summary = MiAttackSummary(self.labels, self.predictions, 'Random', 'Groundhog')
 
     def test_accuracy(self):
-        self.assertEqual(self.attack_summary.accuracy, 0.49)
+        self.assertEqual(self.attack_summary.accuracy, np.mean(self.predictions == self.labels))
+
+    def test_fp(self):
+        self.assertEqual(self.attack_summary.fp, np.sum(self.predictions[np.where(self.labels == 0)[0]] == 1)/len(np.where(self.labels == 0)[0]))
+
+    def test_tp(self):
+        self.assertEqual(self.attack_summary.tp, np.sum(self.predictions[np.where(self.labels == 1)[0]] == 1)/len(np.where(self.labels == 1)[0]))
