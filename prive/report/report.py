7e53c9541057eb641fb6b2113b23b382daf4450c
crangelsmith <crangelsmith@turing.ac.uk>
2022-05-12 15:47:11 +0100

    adding report classes

--- prive/report/report.py
+++ prive/report/report.py
@@ -1,8 +1,10 @@
 """Classes to report summary of attacks"""
 from abc import ABC, abstractmethod
 from prive.report import MIAttackSummary
+import os
+from prive.utils.plots import metric_comparison_plots
 import pandas as pd
-
+import datetime
 
 class Report(ABC):
     @classmethod
@@ -36,10 +38,17 @@ class Report(ABC):
 
         """
         pass
+    @abstractmethod
+    def compare_datasets(self, output_path):
+        """
+        Plot different datasets for same generator-attacks.
+
+        """
+        pass
 
 
 class MIAttackReport(Report):
-    def __init__(self, df):
+    def __init__(self, df, metrics):
         """
         Initialise class
 
@@ -51,9 +60,11 @@ class MIAttackReport(Report):
 
         """
         self.attacks_data = df
+        self.metrics = metrics
 
     @classmethod
-    def load_summary_statistics(cls, attacks):
+    def load_summary_statistics(cls, attacks,
+                                metrics=None):
         """
         Load attacks data, calculate summary statistics, merge into a single dataframe and initialise object.
 
@@ -61,6 +72,7 @@ class MIAttackReport(Report):
         ----------
         attacks: list[dicts]
             List of dictionaries with results of attacks
+        metrics: list[str]
 
         Returns
         -------
@@ -68,31 +80,111 @@ class MIAttackReport(Report):
 
         """
 
+        if metrics is None:
+            metrics = ["accuracy", "true_positive_rate", "false_positive_rate", "mia_advantage",
+                       "privacy_gain"]
         df_list = []
 
         for attack in attacks:
-
             mia_summary = MIAttackSummary(
                 attack["labels"],
                 attack["predictions"],
-                attack["generator_info"],
-                attack["attack_info"],
-                attack["dataset_info"],
+                attack["generator"],
+                attack["attack"],
+                attack["dataset"],
                 attack["target_id"],
             ).get_metrics()
 
             df_list.append(mia_summary)
 
-        return cls(pd.concat(df_list))
+        return cls(pd.concat(df_list), metrics)
 
-    def compare_generators(self, output_path):
+    def compare_generators(self, filepath):
+        """
+        For each pair of datasets-attacks available in the data make a figure comparing performance between
+        different generators and metrics. Figures are saved to disk.
 
-        pass
+        Parameters
+        ----------
+        filepath: str
+            Path where the figure is to be saved.
 
-    def compare_attacks(self, output_path):
+        Returns
+        -------
+        None
+        """
 
-        pass
+        metric_comparison_plots(data=self.attacks_data, comparison_label='generator', pairs_label=['dataset', 'attack'],
+                                metrics=self.metrics, targets_label='target_id',
+                                output_path=filepath)
 
-    def compare_datasets(self, output_path):
+        return None
 
-        pass
+    def compare_attacks(self, filepath):
+
+        """
+
+        For each pair of datasets-generators available in the data make a figure comparing performance between
+        different attacks and metrics. Figures are saved to disk.
+
+       Parameters
+        ----------
+        filepath: str
+            Path where the figure is to be saved.
+
+        Returns
+        -------
+        None
+
+        """
+
+        metric_comparison_plots(data=self.attacks_data, comparison_label='attack', pairs_label=['dataset', 'generator'],
+                                metrics=self.metrics, targets_label='target_id',
+                                output_path=filepath)
+
+        return None
+
+    def compare_datasets(self, filepath):
+
+        """
+
+        For each pair of attacks-generators available in the data make a figure comparing performance between
+        different datasets and metrics. Figures are saved to disk.
+
+       Parameters
+        ----------
+        filepath: str
+            Path where the figure is to be saved.
+
+        Returns
+        -------
+        None
+
+        """
+
+        metric_comparison_plots(data=self.attacks_data, comparison_label='dataset', pairs_label=['attack', 'generator'],
+                                metrics=self.metrics, targets_label='target_id',
+                                output_path=filepath)
+
+        return None
+
+    def create_report(self,filepath):
+        """
+        Make all comparison plots and save them to disk.
+
+        Parameters
+        ----------
+        filepath: str
+            Path where the figure is to be saved.
+
+        Returns
+        -------
+        None
+
+        """
+
+        self.compare_generators(filepath)
+        self.compare_attacks(filepath)
+        self.compare_datasets(filepath)
+
+        print (f'All figures saved to directory {filepath}')
