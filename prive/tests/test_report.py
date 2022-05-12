56a774225351cabb3747527ac2c44087375ecd78
crangelsmith <crangelsmith@turing.ac.uk>
2022-05-12 15:47:16 +0100

    adding report tests

--- prive/tests/test_report.py
+++ prive/tests/test_report.py
@@ -1,6 +1,7 @@
 import os
 import unittest
 import numpy as np
+import datetime
 from prive.report import MIAttackReport
 
 
@@ -10,14 +11,14 @@ class MIAReport(unittest.TestCase):
         np.random.seed(0)
 
         attacks = []
-        for i in range(10):
+        for i in range(1000):
             attack_dict = {}
             attack_dict['labels'] = np.random.randint(2, size=100)
             attack_dict['predictions'] = np.random.randint(2, size=100)
-            attack_dict['generator_info'] = 'Random'
-            attack_dict['dataset_info'] = 'Test'
-            attack_dict['target_id'] = 1
-            attack_dict['attack_info'] = 'RandomGuess'
+            attack_dict['generator'] = np.random.choice(['RandomGenerator1','RandomGenerator2','RandomGenerator3'])
+            attack_dict['dataset'] = np.random.choice(['TestData1'])
+            attack_dict['target_id'] = np.random.randint(1,5,size=1)[0]
+            attack_dict['attack'] = np.random.choice(['RandomGuess1','RandomGuess2'])
 
             attacks.append(attack_dict)
 
@@ -25,3 +26,16 @@ class MIAReport(unittest.TestCase):
 
     def test_setup(self):
         self.assertTrue(self.report.attacks_data.shape, [9, 10])
+
+    def test_create_report(self):
+
+        filepath_timestamp = os.path.join(os.path.dirname(__file__), f'prive_report_{datetime.datetime.now().strftime("%Y%m%d-%H%M%S")}')
+
+        self.report.create_report(filepath_timestamp)
+
+        self.assertTrue(os.path.exists(filepath_timestamp))
+
+        n_figures = len([f for f in os.listdir(filepath_timestamp) if os.path.isfile(os.path.join(filepath_timestamp, f))])
+
+        self.assertEqual(n_figures,11)
+
